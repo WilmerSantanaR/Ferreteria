0528e6ec9d0b25c40eee5efa4b111def3e4dd614
WilmerSantanaR <wasantana1@misena.edu.co>
2023-08-17 16:58:37 -0500

    Ferreteria

---
+++ compartido/menu.php
@@ -0,0 +1,28 @@
+<?php
+echo '
+<div class="encabezado">
+<header>
+    <div class = titulo>
+    <h1>FERRETERIA MEISSEN</h1>
+    </div>    
+    <div class="logo" >
+    <img src="../productosDavid/imagenesDavid/ferreteria.jpeg" alt="logo ferreteria">
+    </div>
+</header>
+<nav class="navbar">
+    <div class="lista">
+    <a href="../productosDavid/index.html" class="CatalogoTodo">Catalogo</a>
+    <a href="#" class="Pintura">Pintura</a>
+    <a href="../catalogo/elctricas.html" class="Electricas">Electricas</a>
+    <a href="#" class="Herramientas_Manuales">Herramientas Manuales</a>
+    <a href="#" class="Accesorios">Accesorios</a>
+    <button class="btn-login">
+    <a class="btn-login"href="../inicio/Roles.html">Iniciar Sesión</a>
+    </button>
+    <button class="btn-login">
+    <a class="btn-login"href="../registro/RegistroCliente.html">Regístrate Gratis</a>
+    </button>
+    </div>
+</nav>
+</div>'
+?>
